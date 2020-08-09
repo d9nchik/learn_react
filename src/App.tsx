0ffbd759804ed4bc6afd108b89eefa80a488aca0
d9nich <dan.galaiko@gmail.com>
2020-08-09 21:49:57 +0300

    Clock has attribute date.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import './App.css';
-
-function Welcome(props: React.AllHTMLAttributes<{ name: string }>) {
-    return <h1>Привіт, {props.name}</h1>;
-}
-
 function App() {
     return (
-        <div>
-            <Welcome name="Василина"/> <Welcome name="Михайло"/> <Welcome name="Вадим"/></div>
+        <Clock/>
     );
 }
 
 type ClockProps = { date: Date };
 
-class Clock extends React.Component<ClockProps, {}>{
+class Clock extends React.Component<any, ClockProps> {
+    constructor(props: Readonly<any>) {
+        super(props);
+        this.state = {
+            date: new Date()
+        }
+    }
 
     render() {
-        return (<div><h1>Привіт, світе!</h1>      <h2>Зараз {this.props.date.toLocaleTimeString()}.</h2></div>)
+        return (<div><h1>Привіт, світе!</h1>      <h2>Зараз {this.state.date.toLocaleTimeString()}.</h2></div>)
     }
 }
-
-function tick() {
-    const element = (
-        <Clock date={new Date()}/>
-    );
-    ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
 
 export default App;
