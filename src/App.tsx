6842d079a1521d31c62583cc7c1c54df22a2ddb8
d9nich <dan.galaiko@gmail.com>
2020-08-09 21:31:05 +0300

    Attempt to create user friendly clock

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom'
 import './App.css';
 
 function Welcome(props: React.AllHTMLAttributes<{ name: string }>) {
@@ -11,5 +12,18 @@ function App() {
             <Welcome name="Василина"/> <Welcome name="Михайло"/> <Welcome name="Вадим"/></div>
     );
 }
+
+function Clock(props: { date: Date }) {
+    return (<div><h1>Привіт, світе!</h1>      <h2>Зараз {props.date.toLocaleTimeString()}.</h2></div>)
+}
+
+function tick() {
+    const element = (
+        <Clock date={new Date()}/>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
 
 export default App;
