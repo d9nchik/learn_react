b4413423daa29badfe187a28a263b4de25dfb5a6
d9nich <dan.galaiko@gmail.com>
2020-08-09 21:11:35 +0300

    Props is a part of love.

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './App.css';
 
-function getNumber(message: string): number {
-    let result = prompt(message);
-    if (result == null)
-        return 0
-    return Number(result);//Work even if user enter not number
+function Welcome(props: React.AllHTMLAttributes<{ name: string }>) {
+    return <h1>Привіт, {props.name}</h1>;
 }
 
 function App() {
     return (
-        <h1 id="red">Hа старт</h1>
+        <div>
+            <Welcome name="Василина"/> <Welcome name="Михайло"/> <Welcome name="Вадим"/></div>
     );
 }
 
-let time = getNumber("Укажите врея для отсчёта?")
-
-function tick() {
-    let element = <p>Время вышло</p>
-    if (time > 0)
-        element = <p>Осталось {time}&nbsp;секунд</p>
-    time--
-    ReactDOM.render(element, document.getElementById("root"))
-}
-
-setInterval(tick, 1_000)
 export default App;
