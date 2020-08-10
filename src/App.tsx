84cc33ac852d220d4523e3dfbe03313aca70a95c
d9nich <dan.galaiko@gmail.com>
2020-08-10 20:52:00 +0300

    Simple app, that uses React events.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <Clock/>
-    );
+function handleClick(e: React.SyntheticEvent) {
+    e.preventDefault()
+    console.log("You clicked on link")
 }
 
-type ClockProps = { date: Date };
+function App() {
+    return <a href="https://google.com" onClick={handleClick}>Click me</a>
 
-class Clock extends React.Component<any, ClockProps> {
-    private timerID?: NodeJS.Timeout;
-
-    constructor(props: Readonly<any>) {
-        super(props);
-        this.state = {
-            date: new Date()
-        }
-    }
-
-
-    componentDidMount() {
-        this.timerID = setInterval(() => this.tick(), 1_000)
-    }
-
-
-    componentWillUnmount() {
-        if (this.timerID instanceof NodeJS.Timeout) {
-            clearInterval(this.timerID)
-        }
-    }
-
-
-    render() {
-        return (<div><h1>Привіт, світе!</h1>      <h2>Зараз {this.state.date.toLocaleTimeString()}.</h2></div>)
-    }
-
-    private tick() {
-        this.setState({date: new Date()})
-    }
 }
 
 export default App;
