4bd04dc86090b00aa39d302eee4edc6e20dddcaa
d9nich <dan.galaiko@gmail.com>
2020-08-10 22:59:28 +0300

    Hiding component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,41 @@
 import React from 'react';
 import './App.css';
 
-function UserGreeting() {
-    return <h1>З поверненням!</h1>;
-}
-
-function GuestGreeting() {
-    return <h1>Зареєструйтеся, будь-ласка.</h1>;
-}
-
-type IsLoggedType = {
-    isLoggedIn: boolean
-}
-
-function Greeting(props: IsLoggedType) {
-    const isLoggedIn = props.isLoggedIn;
-    if (isLoggedIn) {
-        return <UserGreeting/>;
-    }
-    return <GuestGreeting/>;
-}
-
-function LoginButton(props: { onClick: () => void }) {
+function WarningBanner(props: {warn: boolean}) {
+    if (!props.warn) {    return null;  }
     return (
-        <button onClick={props.onClick}>
-            Увійти
-        </button>
+        <div className="warning">
+            Попередження!
+        </div>
     );
 }
 
-function LogoutButton(props: { onClick: () => void }) {
-    return (
-        <button onClick={props.onClick}>
-            Вийти
-        </button>
-    );
-}
-
-class LoginControl extends React.Component<any, IsLoggedType> {
+class Page extends React.Component<any, {showWarning: boolean}> {
     constructor(props: any) {
         super(props);
-        this.handleLoginClick = this.handleLoginClick.bind(this);
-        this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
+        this.state = {showWarning: true};
+        this.handleToggleClick = this.handleToggleClick.bind(this);
     }
 
-    handleLoginClick() {
-        this.setState({isLoggedIn: true});
-    }
-
-    handleLogoutClick() {
-        this.setState({isLoggedIn: false});
+    handleToggleClick() {
+        this.setState(state => ({
+            showWarning: !state.showWarning
+        }));
     }
 
     render() {
-        const isLoggedIn = this.state.isLoggedIn;
-        let button;
-        if (isLoggedIn) {
-            button = <LogoutButton onClick={this.handleLogoutClick}/>;
-        } else {
-            button = <LoginButton onClick={this.handleLoginClick}/>;
-        }
         return (
             <div>
-                <Greeting isLoggedIn={isLoggedIn}/> {button}      </div>
+                <WarningBanner warn={this.state.showWarning} />        <button onClick={this.handleToggleClick}>
+                {this.state.showWarning ? 'Сховати' : 'Показати'}
+            </button>
+            </div>
         );
     }
 }
 
 function App() {
-    return <LoginControl/>
+    return <Page/>
 }
 
 export default App;
