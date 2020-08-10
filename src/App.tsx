ade207484a6011d63ed69e11fff7d7fb3d591039
d9nich <dan.galaiko@gmail.com>
2020-08-10 21:51:30 +0300

    More complicated example of conditional rendering, that uses React.

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ function GuestGreeting() {
     return <h1>Зареєструйтеся, будь-ласка.</h1>;
 }
 
-type greet = {
+type IsLoggedType = {
     isLoggedIn: boolean
 }
 
-function Greeting(props: greet) {
+function Greeting(props: IsLoggedType) {
     const isLoggedIn = props.isLoggedIn;
     if (isLoggedIn) {
         return <UserGreeting/>;
@@ -21,8 +21,55 @@ function Greeting(props: greet) {
     return <GuestGreeting/>;
 }
 
+function LoginButton(props: { onClick: () => void }) {
+    return (
+        <button onClick={props.onClick}>
+            Увійти
+        </button>
+    );
+}
+
+function LogoutButton(props: { onClick: () => void }) {
+    return (
+        <button onClick={props.onClick}>
+            Вийти
+        </button>
+    );
+}
+
+class LoginControl extends React.Component<any, IsLoggedType> {
+    constructor(props: any) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = {isLoggedIn: false};
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick() {
+        this.setState({isLoggedIn: false});
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let button;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick}/>;
+        } else {
+            button = <LoginButton onClick={this.handleLoginClick}/>;
+        }
+        return (
+            <div>
+                <Greeting isLoggedIn={isLoggedIn}/> {button}      </div>
+        );
+    }
+}
+
 function App() {
-    return <Greeting isLoggedIn={true}/>
+    return <LoginControl/>
 }
 
 export default App;
