425d0d80b2b52fb182c12466834eb37476c422c7
d9nich <dan.galaiko@gmail.com>
2020-08-11 21:32:36 +0300

    Hiding component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function WarningBanner(props: {warn: boolean}) {
-    if (!props.warn) {    return null;  }
+const numbers = [1, 2, 3, 4, 5];
+
+function NumberList(props: { numbers: number[] }) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number: number) => <li key={number.toString()}>{number}</li>);
     return (
-        <div className="warning">
-            Попередження!
-        </div>
-    );
-}
-
-class Page extends React.Component<any, {showWarning: boolean}> {
-    constructor(props: any) {
-        super(props);
-        this.state = {showWarning: true};
-        this.handleToggleClick = this.handleToggleClick.bind(this);
-    }
-
-    handleToggleClick() {
-        this.setState(state => ({
-            showWarning: !state.showWarning
-        }));
-    }
-
-    render() {
-        return (
-            <div>
-                <WarningBanner warn={this.state.showWarning} />        <button onClick={this.handleToggleClick}>
-                {this.state.showWarning ? 'Сховати' : 'Показати'}
-            </button>
-            </div>
-        );
-    }
+        <ul>{listItems}</ul>);
 }
 
 function App() {
-    return <Page/>
+    return <NumberList numbers={numbers}/>
 }
 
 export default App;
