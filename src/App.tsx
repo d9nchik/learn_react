830188d67b27ed5e868020cb09fd43d08f630d6c
d9nich <dan.galaiko@gmail.com>
2020-08-12 15:51:54 +0300

    Half of synchronized calculator has been done.

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,20 @@ function BoilingVerdict(props: { celsius: number }) {
 }
 
 class Calculator extends React.Component<any, { temperature: string }> {
+
+    render() {
+        return (
+            <div>
+                <TemperatureInput scale="c"/>
+                <TemperatureInput scale="f"/>
+            </div>
+        );
+    }
+}
+
+const scaleNames = {c: 'Цельсій', f: 'Фаренгейт'};
+
+class TemperatureInput extends React.Component<{ scale: 'c' | 'f' }, { temperature: string }> {
     constructor(props: any) {
         super(props);
         this.handleChange = this.handleChange.bind(this);
@@ -22,13 +36,33 @@ class Calculator extends React.Component<any, { temperature: string }> {
 
     render() {
         const temperature = this.state.temperature;
+        const scale = this.props.scale;
         return (
             <fieldset>
-                <legend>Введіть температуру в градусах Цельсія:</legend>
-                <input value={temperature} onChange={this.handleChange}/> <BoilingVerdict
-                celsius={parseFloat(temperature)}/></fieldset>
+                <legend>Введіть температуру в градусах {scaleNames[scale]}:</legend>
+                <input value={temperature}
+                       onChange={this.handleChange}/>
+            </fieldset>
         );
     }
+}
+
+function toCelsius(fahrenheit: number) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius: number) {
+    return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(temperature: string, convert: (a: number) => number): string {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
 }
 
 function App() {
